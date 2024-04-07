66469a904c394d8c1881dc1344cfabd76b9d0345
sirx2713 <bradx2713@gmail.com>
2024-04-07 11:50:41 +0200

    Searchable Sushi

--- menu_D32/SushiView.swift
+++ menu_D32/SushiView.swift
@@ -13,46 +13,61 @@ struct SushiView: View {
     @State var selectedSushiImage = ""
     @State var selectedSushi = ""
     @State var selectedSushiPrice = ""
+    @State var searchSushi = ""
     @State var sushiSheet = false
     
+    var filteredSushi: [SushiLogic] {
+        guard !searchSushi.isEmpty else { return sushiList}
+        return sushiList.filter { sushi in
+            sushi.name.lowercased().contains(searchSushi.lowercased())
+        }
+    }
     var getData = DataServices()
     
+    
     var body: some View {
-        List(sushiList){sushi in
-            HStack{
-                Image(sushi.imageName)
-                    .resizable()
-                    .scaledToFit()
-                    .cornerRadius(10)
-                    .frame(width: 58, height: 58)
-                Text(sushi.name)
-                    .fontWeight(.semibold)
-                Spacer()
-                Text("$\(sushi.price)")
+        NavigationStack{
+            List(filteredSushi){
+                sushi in
+                HStack{
+                    Image(sushi.imageName)
+                        .resizable()
+                        .scaledToFit()
+                        .cornerRadius(10)
+                        .frame(width: 58, height: 58)
+                    Text(sushi.name)
+                        .fontWeight(.semibold)
+                    Spacer()
+                    Text("$\(sushi.price)")
+                }
+                .onTapGesture {
+                    selectedSushiImage = sushi.imageName
+                    selectedSushi = sushi.name
+                    selectedSushiPrice = sushi.price
+                    sushiSheet = true
+                }
+                .listRowBackground(
+                    LinearGradient(colors: [Color("CreamLight"), Color("CreamDark")], startPoint: .topLeading, endPoint: .bottomTrailing)
+                )
+                
             }
-            .onTapGesture {
-                selectedSushiImage = sushi.imageName
-                selectedSushi = sushi.name
-                selectedSushiPrice = sushi.price
-                sushiSheet = true
-            }
-            .listRowBackground(
-                LinearGradient(colors: [Color("CreamLight"), Color("CreamDark")], startPoint: .topLeading, endPoint: .bottomTrailing)
+            .listStyle(.plain)
+            .navigationTitle(
+                Text("Sushi Menu")
             )
-            
+            .searchable(text: $searchSushi, prompt: "Search Sushi")
+            .sheet(isPresented: $sushiSheet, content: {
+                SushiSheet(selectedSushiImage: $selectedSushiImage, selectedSushi: $selectedSushi, selectedSushiPrice: $selectedSushiPrice, sushiSheet: $sushiSheet)
+                    .presentationDragIndicator(.visible)
+                    .presentationDetents([.medium, .large])
+            })
+            .onAppear(perform: {
+                sushiList = getData.sushiViewData()
+            })
         }
-        .listStyle(.plain)
-        .sheet(isPresented: $sushiSheet, content: {
-            SushiSheet(selectedSushiImage: $selectedSushiImage, selectedSushi: $selectedSushi, selectedSushiPrice: $selectedSushiPrice, sushiSheet: $sushiSheet)
-                .presentationDragIndicator(.visible)
-                .presentationDetents([.medium, .large])
-        })
-        .onAppear(perform: {
-            sushiList = getData.sushiViewData()
-        })
     }
+    
 }
-
 #Preview {
     SushiView()
 }
